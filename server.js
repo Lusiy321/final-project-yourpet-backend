6993f73ba02acd95787e7bafe9ce49834333fc19
VOLODYMYR LYSENKO <107991551+Lusiy321@users.noreply.github.com>
2023-05-30 20:33:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatsLogger));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/posts", postRouter);
 app.use("/users", usrRouter);
